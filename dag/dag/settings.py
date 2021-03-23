d319c5bf21c49a698d65160f9e76d7f036222577
lythari <davidalexandregiraud@gmail.com>
2021-03-23 15:47:44 +0100

    Fix: use python-dotenv properly

--- dag/dag/settings.py
+++ dag/dag/settings.py
@@ -10,12 +10,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
-from os import getenv
+import os
 from pathlib import Path
-from dotenv import load_dotenv
 
-env_path = Path('.') / '.env'
-load_dotenv(dotenv_path=env_path)
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -24,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = getenv("SALT")
+SECRET_KEY = os.getenv("SALT")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
--- dag/dag/wsgi.py
+++ dag/dag/wsgi.py
@@ -10,7 +10,11 @@ https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+from pathlib import Path
+from dotenv import load_dotenv
 
+env_path = Path('.') / '.env'
+load_dotenv(dotenv_path=env_path)
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dag.settings')
 
 application = get_wsgi_application()
