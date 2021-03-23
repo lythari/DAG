d2013a1aefe66a594a779e5befeabd91f0b39055
lythari <davidalexandregiraud@gmail.com>
2021-03-23 21:31:06 +0100

    Feature: Add initial models

--- dag/navbar/admin.py
+++ dag/navbar/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from django.db.models import Count
 
-# Register your models here.
+import navbar.models as navbar_models
+
+
+class LinksInline(admin.TabularInline):
+    model = navbar_models.Link
+    extra = 10
+
+
+@admin.register(navbar_models.Navbar)
+class NavbarAdmin(admin.ModelAdmin):
+    list_filter = ('state',)
+    list_display = ('state', 'position')
+    inlines = [LinksInline]
--- dag/navbar/models.py
+++ dag/navbar/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from django.template.defaultfilters import slugify
 
-# Create your models here.
+
+class Navbar(models.Model):
+    CHOICES = (
+        ('VL', 'Vertical Left'),
+        ('VR', 'Vertical Right'),
+        ('H', 'Horizontal'),
+    )
+    state = models.BooleanField(default=False, verbose_name="Active")
+    position = models.CharField(max_length=64, choices=CHOICES)
+
+    class Meta:
+        verbose_name = "Barre de navigation"
+
+
+class Link(models.Model):
+
+    bar = models.ForeignKey(Navbar, on_delete=models.CASCADE)
+    name = models.CharField(max_length=64, unique=True)
+    slug = models.SlugField(max_length=255, blank=True)
+    position = models.IntegerField()
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+
+        super().save(*args, **kwargs)
+
+    class Meta:
+        ordering = ['position']
+        verbose_name = "Lien"
