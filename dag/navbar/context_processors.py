f30bd534ea6aede1825ba881528595fa5d61688f
lythari <davidalexandregiraud@gmail.com>
2021-03-24 11:52:36 +0100

    Feature: remove position filter on bar

--- dag/navbar/context_processors.py
+++ dag/navbar/context_processors.py
@@ -7,7 +7,7 @@ def navbar(request):
     """Add the navbar specified in the settings to the context."""
 
     if 'navbar' in settings.INSTALLED_APPS:
-        h_bar = navbar_models.Navbar.objects.filter(state=True).filter(position='H').first()
-        if h_bar:
-            return {"navbar": h_bar}
+        bar = navbar_models.Navbar.objects.filter(state=True).first()
+        if bar:
+            return {"navbar": bar}
     return {}
